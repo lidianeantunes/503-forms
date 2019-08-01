647542b93a291e1844604b83c5590da306c329f6
lidiane <lidianeantunes346@gmail.com>
2019-08-01 14:19:05 -0300

    adicionar forms

---
+++ script.js
@@ -0,0 +1,12 @@
+let nome = document.querySelector('#noeme');
+let email = document.querySelector('#email');
+let confirme = document.querySelector('#confirme');
+let senha = document.querySelector('#senha');
+let sexo = document.querySelector('#sexo');
+
+if(email == confirme ){
+    console.log (`${email} ,confirmado`)
+}
+function tudo() {
+    
+}
